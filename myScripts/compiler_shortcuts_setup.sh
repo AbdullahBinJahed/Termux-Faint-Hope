e61a73959dddd20ddd8c5e505fa57bd42ba6e7ec
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-18 20:49:40 +0600

    fixed .bashrc path

--- myScripts/compiler_shortcuts_setup.sh
+++ myScripts/compiler_shortcuts_setup.sh
@@ -3,5 +3,6 @@ if [ ! -e $HOME/.lolicon ]; then mkdir $HOME/.lolicon; fi
 rm $HOME/.lolicon/compiler_shortcuts.sh
 mv compiler_shortcuts.sh $HOME/.lolicon/
 als="alias c='source /data/data/com.termux/files/home/.lolicon/compiler_shortcuts.sh'"
-echo "$als" >> $HOME.bashrc
+echo "$als" >> $HOME/.bashrc
+echo -e "\033[01;32mInstallation complete now run c --help to see info\033[00m"
 rm compiler_shortcuts_setup.sh
