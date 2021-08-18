2463074e1b7bacc780142c163fc46de033c54b01
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-19 01:40:51 +0600

    changed setup to avoid multiple alias

--- myScripts/compiler_shortcuts_setup.sh
+++ myScripts/compiler_shortcuts_setup.sh
@@ -4,12 +4,14 @@ BGREEN='\033[01;32m'
 
 echo "${BGREEN}Setup started...${NONE}"
 wget https://raw.githubusercontent.com/AbdullahBinJahed/Termux/main/myScripts/compiler_shortcuts.sh 1>&2 2>/dev/null
-sleep 2s
+sleep 1s
 echo "${BGREEN}Script download complete${NONE}"
 if [ ! -e "$HOME/.lolicon" ]; then mkdir "$HOME/.lolicon"; fi
 rm "$HOME/.lolicon/compiler_shortcuts.sh"
 mv "compiler_shortcuts.sh" "$HOME/.lolicon/"
-als="alias c='source /data/data/com.termux/files/home/.lolicon/compiler_shortcuts.sh'"
-echo "$als" >> "$HOME/.bashrc"
+if [ ! -e $HOME/.lolicon/compiler_shortcuts.sh ]; then
+  als="alias c='source /data/data/com.termux/files/home/.lolicon/compiler_shortcuts.sh'"
+  echo "$als" >> "$HOME/.bashrc"
+fi
 echo "${BGREEN}Setup complete. Now run 'c --help' to see info${NONE}"
 rm compiler_shortcuts_setup.sh
