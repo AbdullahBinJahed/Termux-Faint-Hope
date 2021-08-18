bdfa8236b14d771083ebb80537111b4aef401c9c
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-18 22:09:55 +0600

    fixed setup message

--- myScripts/compiler_shortcuts_setup.sh
+++ myScripts/compiler_shortcuts_setup.sh
@@ -5,5 +5,5 @@ rm "$HOME/.lolicon/compiler_shortcuts.sh"
 mv "compiler_shortcuts.sh" "$HOME/.lolicon/"
 als="alias c='source /data/data/com.termux/files/home/.lolicon/compiler_shortcuts.sh'"
 echo "$als" >> "$HOME/.bashrc"
-echo -e "\033[01;32mInstallation complete now run c --help to see info\033[00m"
+echo "\033[01;32mInstallation complete now run c --help to see info\033[00m"
 rm compiler_shortcuts_setup.sh
