f5eb987b918607873e649fd57e43050a69956453
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-18 22:17:07 +0600

    fixed setup message

--- myScripts/compiler_shortcuts_setup.sh
+++ myScripts/compiler_shortcuts_setup.sh
@@ -2,12 +2,12 @@
 NONE='\033[00m'
 BGREEN='\033[01;32m'
 
-echo -e "${BGREEN}Installation started...${NONE}"
+echo "${BGREEN}Installation started...${NONE}"
 wget https://raw.githubusercontent.com/AbdullahBinJahed/Termux/main/myScripts/compiler_shortcuts.sh
 if [ ! -e "$HOME/.lolicon" ]; then mkdir "$HOME/.lolicon"; fi
 rm "$HOME/.lolicon/compiler_shortcuts.sh"
 mv "compiler_shortcuts.sh" "$HOME/.lolicon/"
 als="alias c='source /data/data/com.termux/files/home/.lolicon/compiler_shortcuts.sh'"
 echo "$als" >> "$HOME/.bashrc"
-echo -e "${BGREEN}Installation finished now run c --help to see info${NONE}"
+echo "${BGREEN}Installation finished now run c --help to see info${NONE}"
 rm compiler_shortcuts_setup.sh
