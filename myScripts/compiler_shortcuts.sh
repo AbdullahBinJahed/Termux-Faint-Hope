89a45b638b7c6ea6c37c6c2db76f13c9de68b873
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-18 21:13:42 +0600

    updated the cmp_shct a little bit more

--- myScripts/compiler_shortcuts.sh
+++ myScripts/compiler_shortcuts.sh
@@ -126,10 +126,10 @@ ArgCheck()
           kill -INT $$
           ;;
         --update )
-          echo -e "${BOLD}${GREEN}Updating...${NONE}"
+          echo -e "${GREEN}${BOLD}Updating...${NONE}"
           wget https://raw.githubusercontent.com/AbdullahBinJahed/Termux/main/myScripts/compiler_shortcuts.sh
           rm $HOME/.lolicon/compiler_shortcuts.sh && mv compiler_shortcuts.sh $HOME/.lolicon/
-          echo -e "${BOLD}${GREEN}Update complete${NONE}"
+          echo -e "${GREEN}${BOLD}Update complete${NONE}"
           kill -INT $$
           ;;
         * )
