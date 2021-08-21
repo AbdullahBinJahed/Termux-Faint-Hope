8a34b9a23e5f3347dbc1b584d5f212db40077ada
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-21 23:02:06 +0600

    changed setup

--- myScripts/setup-cmp-shct.sh
+++ myScripts/setup-cmp-shct.sh
@@ -7,9 +7,10 @@ echo "${BGREEN}Adding the its-pointless repo to install gcc...${NONE}"
 wget https://its-pointless.github.io/setup-pointless-repo.sh 1>&2 2>/dev/null
 chmod +x setup-pointless-repo.sh
 ./setup-pointless-repo.sh
+rm setup-pointless-repo.sh
 echo "${BGREEN}Installing gcc-11...${NONE}"
 pkg install gcc-11
-echo "${BGREEN}Downloading cmp-shct"
+echo "${BGREEN}Downloading cmp-shct${NONE}"
 wget https://raw.githubusercontent.com/AbdullahBinJahed/Termux/main/myScripts/cmp-shct.sh 1>&2 2>/dev/null
 sleep 1s
 echo "${BGREEN}Script download complete${NONE}"
@@ -21,4 +22,4 @@ fi
 rm "$HOME/.lolicon/cmp-shct.sh" 2>/dev/null
 mv cmp-shct.sh "$HOME/.lolicon/"
 echo "${BGREEN}Setup complete. Now run 'c --help' to see info${NONE}"
-rm cmp-shct-setup.sh
+rm setup-cmp-shct.sh
