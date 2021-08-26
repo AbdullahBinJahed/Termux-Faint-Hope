26bd965f3770e86328f28f526dfead221a26d7ad
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-26 13:51:37 +0600

    updated setup-lolicon

--- myScripts/setup-lolicon.sh
+++ myScripts/setup-lolicon.sh
@@ -3,37 +3,44 @@ NONE='\033[00m'
 BGREEN='\033[01;32m'
 
 clear
-echo "${BGREEN}Setup started...${NONE}"
-pkg update && pkg upgrade
-apt update && apt upgrade
-echo "${BGREEN}Installing packages...${NONE}"
-pkg install clang vim nodejs python python3 cmatrix curl wget fish gh git gtk3 ncurses cmake make man neovim net-tools tree vim-gtk x11-repo yarn zsh -y
-echo "${BGREEN}Package Installation Completed${NONE}"
+echo -e "${BGREEN}Setup started...${NONE}"
+pkg update -y
+pkg upgrade -y
+apt update -y
+apt upgrade -y
+echo -e "${BGREEN}Installing packages...${NONE}"
+pkg install clang vim nodejs python cmatrix curl wget fish gh git ncurses cmake make man neovim net-tools tree x11-repo yarn zsh -y
+echo -e "${BGREEN}Package Installation Completed${NONE}"
 mkdir ~/.lolicon
 mkdir ~/.lolicon/scripts
-echo "${BGREEN}Cloning AbdullahBinJahed/Termux...${NONE}"
+echo -e "${BGREEN}Cloning AbdullahBinJahed/Termux...${NONE}"
 git clone https://github.com/AbdullahBinJahed/Termux.git
 cd Termux/myScripts/
 chmod +x setup-cmp-shct.sh
-./setup-cmp-shct.sh
+source setup-cmp-shct.sh
+echo -e "${BGREEN}Installing gtk3 and vim-gtk...${NONE}"
+pkg install gtk3 vim-gtk
 mv -f welcome.sh ~/.lolicon
 mv -f ghpus.sh ~/.lolicon/scripts
 cd ../DOTFILES/
-echo "${BGREEN}Copying DOTFILES...${NONE}"
-mv -rf .config ~/
-mv -rf .local ~/
-mv -rf .termux ~/
-mv -rf .vim ~/
-mv -rf .bashrc ~/
-mv -rf .vimrc ~/
-mv -rf .viminfo ~/
-echo "${BGREEN}Removed Startup message...${NONE}"
-mv /data/data/com.termux/files/usr/etc/motd /data/data/com.termux/files/usr/etc/__not_needed_motd
+echo -e "${BGREEN}Copying DOTFILES...${NONE}"
+cp -rf .config ~/
+cp -rf .local ~/
+cp -rf .termux ~/
+cp -rf .vim ~/
+cp -rf .bashrc ~/
+cp -rf .vimrc ~/
+cp -rf .viminfo ~/
+cp -rf .lolicon ~/
+echo -e "${BGREEN}Installing vim plugin...${NONE}"
+vim +'PlugInstall --sync' +qa
+echo -e "${BGREEN}Removed Startup message...${NONE}"
+rm /data/data/com.termux/files/usr/etc/motd
 cd ../../
-echo "${BGREEN}Deleting AbdullahBinJahed/Termux...${NONE}"
+echo -e "${BGREEN}Deleting AbdullahBinJahed/Termux...${NONE}"
 rm -rf Termux
-echo "${BGREEN}Changing default shell from bash to fish...${NONE}"
+echo -e "${BGREEN}Changing default shell from bash to fish...${NONE}"
 chsh -s fish
-echo "${BGREEN}Setup Completed${NONE}"
+echo -e "${BGREEN}Setup Completed${NONE}"
 termux-reload-settings
 exit
