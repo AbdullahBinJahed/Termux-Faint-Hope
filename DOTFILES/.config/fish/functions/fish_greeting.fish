86400a6ed0323e7975779252d1f21a71afbd9cae
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-13 00:47:41 +0600

    updated greetings

--- DOTFILES/.config/fish/functions/fish_greeting.fish
+++ DOTFILES/.config/fish/functions/fish_greeting.fish
@@ -1,3 +1,7 @@
 function fish_greeting
-  bash /data/data/com.termux/files/home/.lolicon/welcome.sh
+  if test (stty size) = "62 70"
+    neofetch
+  else
+    bash /data/data/com.termux/files/home/.lolicon/welcome.sh
+  end
 end
