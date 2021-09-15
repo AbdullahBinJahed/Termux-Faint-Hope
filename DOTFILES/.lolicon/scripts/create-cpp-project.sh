3eb3d75a49e05d4e30a8d9a00ee3a3839743b166
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-15 22:32:25 +0600

    updated create-cpp-project

--- DOTFILES/.lolicon/scripts/create-cpp-project.sh
+++ DOTFILES/.lolicon/scripts/create-cpp-project.sh
@@ -1,13 +1,20 @@
-#!/data/data/com.termux/files/usr/bin/fish
+#!/bin/fish
 
 if test $argv[1] = "-r"
-  rm -rf ~/CPP/Cpp_Log/"$argv[2]" 2>/dev/null
-  rm -rf ~/C++/"$argv[2]" 2>/dev/null
-  cd ..
+  if test -e ~/C++/"$argv[2]"
+    rm -rf ~/CPP/Cpp_Log/"$argv[2]"
+    rm -rf ~/C++/"$argv[2]"
+  else
+    echo "Project doesn't exists !!!"
+  end
 else
-  mkdir ~/CPP/Cpp_Log/"$argv[1]" 2>/dev/null
-  mkdir ~/C++/"$argv[1]" 2>/dev/null
-  cd ~/C++/"$argv[1]"/
-  touch main.cpp
-  vim main.cpp
+  if test -e ~/C++/"$argv[1]"
+    echo "Project already exists !!!"
+  else
+    mkdir ~/CPP/Cpp_Log/"$argv[1]"
+    mkdir ~/C++/"$argv[1]"
+    cd ~/C++/"$argv[1]"/
+    touch main.cpp
+    vim main.cpp
+  end
 end
