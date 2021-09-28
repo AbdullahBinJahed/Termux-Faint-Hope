2d859bd1c8192cb02cb6ee0f5c6a11d2de523473
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-29 00:55:58 +0600

    updated ghpush to try to auto-connect to network

--- DOTFILES/.lolicon/scripts/ghpush.sh
+++ DOTFILES/.lolicon/scripts/ghpush.sh
@@ -1,5 +1,21 @@
 #bin/bash
 
+MAIN()
+{
+  CPP_PATH=/data/data/com.termux/files/home/C++*
+  if [[ $(pwd) = $CPP_PATH ]]; then
+    CPP_GIT
+  else
+    echo -e "\033[01;33mLast commit: $(git log --format=%B -n 1 $(git log -1 --pretty=format:"%h"))\033[00m"
+    git add .
+    echo -e "commit message: \c"
+    read commit_message
+    git commit -m "$commit_message"
+    git push -u origin main
+    echo -e "\033[01;36mSuki desu~ ABJ-sama ヽ(‘ ∇‘ )ノ ~nya\033[00m"
+  fi
+}
+
 CPP_GIT()
 {
   cd /data/data/com.termux/files/home/C++/
@@ -23,16 +39,23 @@ CPP_GIT()
   echo -e "$motd"
 }
 
-CPP_PATH=/data/data/com.termux/files/home/C++*
-if [[ $(pwd) = $CPP_PATH ]]; then
-  CPP_GIT
-else
-  echo -e "\033[01;33mLast commit: $(git log --format=%B -n 1 $(git log -1 --pretty=format:"%h"))\033[00m"
-  git add .
-  echo -e "commit message: \c"
-  read commit_message
-  git commit -m "$commit_message"
-  git push -u origin main
-  echo -e "\033[01;36mSuki desu~ ABJ-sama ヽ(‘ ∇‘ )ノ ~nya\033[00m"
-fi
+CHECK_NETWORK()
+{
+  case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+    [23]) MAIN;;
+    5) echo "The web proxy won't let us through";;
+    *) echo -e "\033[01;31mNo Internet connection or it's very slow (〒﹏〒)\033[00m"
+       echo
+       echo -e "\033[01;32mTrying to connect to wifi...\033[00m"
+       termux-wifi-enable true
+       sleep 3
+       if ping -q -c 1 -W 3 google.com > /dev/null 2>&1; then
+         echo -e "\033[01;32mThe network is up. Retrying... ヽ(*⌒▽⌒*)ﾉ\033[00m"
+         CHECK_NETWORK
+       else
+         echo -e "\033[01;31mThe network is down. Please connect to a network (×﹏×)\033[00m"
+       fi;;
+  esac
+}
 
+CHECK_NETWORK
