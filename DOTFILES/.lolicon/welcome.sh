dbf45967bed5dcd0987c9bff0d96d49c90d40809
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-10-10 08:44:34 +0600

    updated welcome.sh

--- DOTFILES/.lolicon/welcome.sh
+++ DOTFILES/.lolicon/welcome.sh
@@ -11,21 +11,23 @@ BOLD='\033[1m'
 
 if [ "$(stty size)" = "62 70" ] || [ "$(stty size)" = "37 70" ]; then
   neofetch
+  warning="\033[01;31m           There has been no GitHub contribution today !!!\033[00m"
 else
   echo
   echo -e ""$GREEN"------------------------------------------------------------------------------------------------------"$NONE""
   echo -e "🌸👯                         👘 "$PURPLE"Terumakkusu, "$CYAN"A "$YELLOW"B "$GREEN"J "$WHITE"- "$WHITE"sama "$RED"e "$YELLOW"yōkoso 👘                             🎎🌸"
   echo -e ""$GREEN"------------------------------------------------------------------------------------------------------"$NONE""
+  warning="\033[01;31m                           There has been no GitHub contribution today !!!\033[00m"
 fi
 
 gitlast=$(sed -n '3p' < /data/data/com.termux/files/home/.lolicon/.datelog)
 today=$(date -I)
 
 if [ $today != $gitlast ];then
-  echo -e "\033[01;31m                           There has been no GitHub contribution today !!!\033[00m"
+  echo -e "$warning"
 fi
 
 echo
 tput sgr0
-play-audio ~/.lolicon/okaeri.flac &
+play ~/.lolicon/okaeri.flac > /dev/null 2>&1 &
 
